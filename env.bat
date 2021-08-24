00bbf1d54a4cd50679773d9033e9d99ca9fa4425
jedeschaud <je.deschaud@gmail.com>
2021-08-24 10:12:12 +0200

    [fix] env variables for Windows

--- env.bat
+++ env.bat
@@ -1,3 +1,3 @@
-set EXTERNAL_ROOT=%cd%/cmake-build-Release/external/install/Release
+set EXTERNAL_ROOT=%cd%/cmake-build-Windows-Release/external/install/Release
 
 set Path=%Path%;%EXTERNAL_ROOT%/Ceres/bin;%EXTERNAL_ROOT%/glog/bin
