53abf5ac0d0eb664e44e32cd31a3252c7e9d6c0c
jedeschaud <je.deschaud@gmail.com>
2021-08-24 10:08:04 +0200

    modifs for building on Windows

--- ct_icp_build.bat
+++ ct_icp_build.bat
@@ -1,8 +1,8 @@
 set SRC_DIR=%cd%
 set CMAKE_GENERATOR="Visual Studio 16 2019"
 set BUILD_CONFIG=Release
-set BUILD_DIR=%SRC_DIR%\cmake-build-%BUILD_CONFIG%
-set WITH_VIZ3D=ON
+set BUILD_DIR=%SRC_DIR%\cmake-build-Windows-%BUILD_CONFIG%
+set WITH_VIZ3D=OFF
 set WITH_PYTHON=ON
 
 mkdir %BUILD_DIR%
@@ -11,7 +11,7 @@ mkdir %BUILD_DIR%\external
 cd %BUILD_DIR%\external
 @echo "[CT_ICP] Building External Dependencies"
 cmake -G %CMAKE_GENERATOR% -S %SRC_DIR%\external -DWITH_VIZ3D=%WITH_VIZ3D%
-cmake --build . --config %BUILD_CONFIG% --target install
+cmake --build . --config %BUILD_CONFIG%
 
 
 @echo "[CT_ICP] Building main project"
@@ -19,5 +19,9 @@ cd %BUILD_DIR%
 cmake -G %CMAKE_GENERATOR% -S %SRC_DIR% -DWITH_VIZ3D=%WITH_VIZ3D% 
 cmake --build . --target install --config %BUILD_CONFIG%
 
+
 cd %SRC_DIR%
+
+
+
 pause
--- env.bat
+++ env.bat
@@ -1,3 +1,3 @@
-EXTERNAL_ROOT=$(pwd)/cmake-build-Release/external/install/Release
+set EXTERNAL_ROOT=%cd%/cmake-build-Release/external/install/Release
 
-set Path=%Path%;%EXTERNAL_ROOT%/Ceres/bin;%EXTERNAL_ROOT%/glog/lib
+set Path=%Path%;%EXTERNAL_ROOT%/Ceres/bin;%EXTERNAL_ROOT%/glog/bin
